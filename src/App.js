fe30870d628989597410199fe153515859d3836a
Shawn Flanigan <shawnmflanigan@gmail.com>
2021-09-12 19:14:40 -0700

    finish .svg and icon rendering

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
-import { Container } from 'react-bootstrap';
-import Col from 'react-bootstrap/Col';
-import Row  from "react-bootstrap/Row";
+import { Container } from "react-bootstrap";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 import SelectFactions from "./components/SelectFactions";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Test from "./assets/icons/Eldar.svg"
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div>
-    <Container fluid>
-      <Row>
-        <Col >
-          <SelectFactions />
-        </Col>
-        <Col xs={6}>
-          <h1>This is where stuff goes</h1>
-          <img src={Test} alt="" />
-        </Col>
-        <Col>
-          <SelectFactions />
-        </Col>
-      </Row>
-    </Container>
+      <Container fluid>
+        <Row>
+          <Col>
+            <SelectFactions />
+          </Col>
+          <Col xs={6}>
+            <h1>This is where stuff goes</h1>
+          </Col>
+          <Col>
+            <SelectFactions />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
